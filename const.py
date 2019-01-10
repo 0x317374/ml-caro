e09a013c0f313f6ad0377f74b045566a67f9d588
DCCXX <dccxxcxix@gmail.com>
2019-01-10 20:08:12 +0700

    keras

--- const.py
+++ const.py
@@ -1,7 +1,7 @@
 class Const:
-  board_width = 3
-  board_height = 3
-  n_in_row = 3  # n to win!
+  board_width = 15
+  board_height = 15
+  n_in_row = 5  # n to win!
   train_core = "keras"
   check_freq = 10  # auto save current model
   check_freq_best = 500  # auto save best model
