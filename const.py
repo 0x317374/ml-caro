20be6c6e23a79580ecdd8da3707ff9b047060734
DCCXX <dccxxcxix@gmail.com>
2019-01-10 14:01:26 +0700

    fix slow save

--- const.py
+++ const.py
@@ -2,7 +2,7 @@ class Const:
   board_width = 19
   board_height = 19
   n_in_row = 5  # n to win!
-  use_gpu = True  # pytorch only!
+  use_gpu = False  # pytorch only!
   train_core = "pytorch"  # pytorch
   check_freq = 10  # auto save current model
   check_freq_best = 500  # auto save best model
