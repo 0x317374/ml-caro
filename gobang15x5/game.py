02788a992e297a1fd52aff897ebc689ce5167e52
DCCXX <dccxxcxix@gmail.com>
2019-01-14 07:58:54 +0700

    gobang6x4

--- gobang15x5/game.py
+++ gobang15x5/game.py
@@ -103,23 +103,23 @@ class GobangGame(Game):
 
 def display(board):
   n = board.shape[0]
+  print("  ", end = "")
   for y in range(n):
-    print(y, "|", end = "")
+    print(y, end = " ")
   print("")
-  print(" -----------------------")
+  # print("---")
   for y in range(n):
-    print(y, "|", end = "")  # print the row #
+    print(y, end = " ")  # print the row #
     for x in range(n):
       piece = board[y][x]  # get the piece to print
       if piece==-1:
-        print("b ", end = "")
+        print("X ", end = "")
       elif piece==1:
-        print("W ", end = "")
+        print("O ", end = "")
       else:
         if x==n:
           print("-", end = "")
         else:
           print("- ", end = "")
-    print("|")
-
-  print("   -----------------------")
+    print("")
+  # print("---")
